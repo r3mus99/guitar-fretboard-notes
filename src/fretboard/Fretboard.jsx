f69e4ea84c56680a13fce16e4a36902a8f835521
Marek Vozárik <37809305+r3mus99@users.noreply.github.com>
2024-02-08 10:33:51 +0000

    feat: Fix border color, add background color to specific cells and refactor className

--- src/fretboard/Fretboard.jsx
+++ src/fretboard/Fretboard.jsx
@@ -36,7 +36,6 @@ export default function Fretboard() {
 
 function Row(props) {
   const { selectedNote } = useContext(Context);
-  const backgrounds = [3, 5, 7, 9, 12];
   const handleClick = (note, rowIndex, index) => {
     if (note) {
       props.onClick(note, rowIndex + 1, index);
@@ -48,9 +47,6 @@ function Row(props) {
         <Cell
           key={index}
           note={note}
-          bold={index === 0}
-          border={index === 1}
-          background={backgrounds.includes(index)}
           selected={isNoteSelected(note, selectedNote)}
           handleClick={() => handleClick(note, props.index, index)}
         />
@@ -60,19 +56,6 @@ function Row(props) {
 }
 
 function Cell(props) {
-  let clName = "Cell";
-  if (props.border) {
-    clName += " Border";
-  }
-  if (props.bold) {
-    clName += " Bold";
-  }
-  if (props.selected) {
-    clName += " Selected";
-  } else if (props.background) {
-    clName += " Background";
-  }
-
   const renderNote = () => {
     const noteWithoutSign = props.note[0];
     const sign = props.note.slice(1);
@@ -86,7 +69,10 @@ function Cell(props) {
   };
 
   return (
-    <td className={clName} onClick={props.handleClick}>
+    <td
+      className={"Cell" + (props.selected ? " Selected" : "")}
+      onClick={props.handleClick}
+    >
       {renderNote()}
     </td>
   );
